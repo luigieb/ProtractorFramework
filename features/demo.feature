f30c3aa67fbb722ee339cf855350ffd530f5669f
Luigi Badin <badinluigi@gmail.com>
2022-05-15 12:53:39 +0300

    second commit

--- features/demo.feature
+++ features/demo.feature
@@ -27,10 +27,15 @@ Feature: Testing Showcase
             | hello |
             | hey   |
 
+    @Banking
+    Scenario Outline: Add customer through bank manager login option
+        Given I launch the url "http://way2automation.com/angularjs-protractor/banking/#/login"
+        When I click on Bank Manager Login
+        And I click on Add customer
+        And I give the customer details "<FirstName>", "<LastName>","<PostCode>"
+        And I click on Add customer button
+        Then I should get the popup
 
-
-
-
-
-
-
+        Examples:
+            | FirstName | LastName | PostCode |
+            | Luigi     | Badin    | 1234     |
--- stepDefinitions/steps.ts
+++ stepDefinitions/steps.ts
@@ -1,16 +1,28 @@
-import { Given, When, Then } from "cucumber";
+import { Given, When, Then, Before } from "cucumber";
 import { calculator } from "../pageObjects/calculator";
 import { browser } from "protractor";
 import { angularHomePage } from "../pageObjects/angularHomePage";
 import chai from "chai";
+import { Home } from "../pageObjects/HomePage";
+import { Addcustomer } from "../pageObjects/AddCustomer";
+
 var expect = chai.expect;
 let calc = new calculator();
 let ah = new angularHomePage();
+let objHome = new Home();
+let objAddCustomer = new Addcustomer();
 
   Given('I will navigate to Calc Site', async () => {
     await browser.get('http://juliemr.github.io/protractor-demo/');
 
   });
+
+  Given('I launch the url {string}', async function (url) {
+    await browser.get(url).then(async function () {
+        await browser.driver.manage().window().maximize();
+        await browser.sleep(5000)
+    })
+  })
 
   Given('I will navigate to {string} page', async (string) => {
     if (string == "calc") {
@@ -36,6 +48,27 @@ let ah = new angularHomePage();
     await calc.secondEditBox.sendKeys(string2);
   });
 
+  When('I click on Bank Manager Login', async function () {
+    await objHome.btnManagerLogin.click();
+});
+
+When('I click on Add customer', async function () {
+    await objAddCustomer.addcustomerLink.click();
+    await browser.sleep(5000)
+});
+
+When('I give the customer details {string}, {string},{string}', async function (fname, lname, postcode) {
+    await objAddCustomer.firstName.sendKeys(fname);
+    await objAddCustomer.lastName.sendKeys(lname);
+    await objAddCustomer.postCode.sendKeys(postcode);
+    await browser.sleep(5000)
+});
+
+When('I click on Add customer button', async function () {
+    await objAddCustomer.AddcustomerButton.click();
+    await browser.sleep(5000)
+});
+
   Then('you will enter {string} in search box', async (string) => {
     await browser.sleep(3000);
     await ah.search.sendKeys(string);
@@ -48,3 +81,23 @@ let ah = new angularHomePage();
       expect(text).to.equal(string);
     })
   });
+
+  Then('I should get the popup', { timeout: 60 * 1000 }, async function () {
+    try {
+
+        await browser.switchTo().alert().getText().then(function (popupmessage) {
+            console.log(popupmessage)
+            browser.switchTo().alert().accept();
+            expect(popupmessage).to.include('Customer added successfully with customer id')
+
+        })
+    } catch (error) {
+        console.log("Exception in Alert: " + error);
+        expect(true).to.equal(false);
+    }
+});
+
+
+Before(async function () {
+    await browser.manage().deleteAllCookies();
+})
